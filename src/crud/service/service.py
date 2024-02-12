a75625de38b0e6525cef2891dbd965b0e296a883
aaryan basnet <aaryanbasnet16@gmail.com>
2024-02-12 20:33:27 +0545

    auth added

--- src/crud/service/service.py
+++ src/crud/service/service.py
@@ -1,4 +1,4 @@
-from src.crud.repository.repository import repo
+from src.user.repository.repository import repo
 from sqlalchemy.orm import Session
 
 class CrudService:
@@ -15,17 +15,7 @@ class CrudService:
     def add(self,data):
         return self.repo.add(item=data)
     
-    def update(self,index,data):
-        data1=self.repo.get(index=index)
-       
-        for key,value in data.items():
-           setattr(data1,key,value)
-        
-        
-        return self.repo.update(change=data1)
     
-    def delete(self,index):
-        return self.repo.delete(index=index)
 
     
 
--- src/entrypoint/main.py
+++ src/entrypoint/main.py
@@ -1,12 +1,17 @@
 from fastapi import FastAPI,status
 from src.crud.router.router import crud
-# from src.blog.router.router import blog
-# from src.convo.router.router import convo
+from src.blog.router.router import blog
+from src.convo.router.router import convo
+from src.user.router.router import user
+from src.auth.router.router import auth
+
 
 app=FastAPI()
 app.include_router(crud)
-# app.include_router(blog)
-# app.include_router(convo)
+app.include_router(blog)
+app.include_router(convo)
+app.include_router(user)
+app.include_router(auth)
 
 
 @app.get(path="/",status_code=status.HTTP_200_OK)
